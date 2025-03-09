74aa5dc2aa6d934d390015cf5443f9467e4494b3
thinkGrow <rubaitreshad@gmail.com>
2025-03-09 16:16:19 +0600

    Clean Code - Explained

--- for_loop.py
+++ for_loop.py
@@ -5,16 +5,16 @@
 #     1. Print numbers from 1 to 10
 # Write a program that prints numbers from 1 to 10 using a for loop.
 
+# for x in range(1,10):
+#     print(x)
+
 # 2. Print even numbers from 1 to 20
 # Modify the previous program to print only even numbers between 1 and 20.
 
 
-
-
 #  Print all odd numbers from 0 - 50.
 
-# for x in range(1,10):
-#     print(x)
+
 
 # for x in range(0,20,2):
 #     print(x)
