9600dfe9ebd4a655368ee1cee246fd5f4e6e9aa1
thinkGrow <rubaitreshad@gmail.com>
2025-03-09 16:07:50 +0600

    Random Generator using iteration

---
+++ random_int.py
@@ -0,0 +1,18 @@
+# Generate 10 random numbers between 1 and 10
+# using : for and while loop
+
+import random
+
+
+# for i in range(10):
+#     x = random.randint(1,11)
+
+#     print(x)
+
+
+
+count = 0
+while count != 10:
+    x = random.randint(1,11)
+    print(x)
+    count = count + 1
