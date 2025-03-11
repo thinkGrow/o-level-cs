7bb8f35f0a1d25ec4530cefc7c712f12f75cc23a
thinkGrow <rubaitreshad@gmail.com>
2025-03-11 16:24:38 +0600

    For Loops - Exercise

---
+++ for-loop-exercise.py
@@ -0,0 +1,23 @@
+
+# write a for loop to print the multiplication table of 7 upto times 10
+
+# print(7*1)
+# print(7*2)
+# print(7*3)
+# print(7*4)
+# print(7*5)
+# print(7*6)
+# print(7*7)
+# print(7*8)
+# print(7*9)
+# print(7*10)
+
+
+# for i in range(1,11,2):
+#     print(7*i)
+
+# square problem
+
+side = int(input("Insert a side of a square: "))
+
+print(f"Are of the square is: {side} * {side} = {side*side}")
