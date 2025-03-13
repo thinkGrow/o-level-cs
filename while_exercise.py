b6fb0425ee7a0c074e2ba13793add9925309a92d
thinkGrow <rubaitreshad@gmail.com>
2025-03-13 16:14:11 +0600

    While Loop - Exercise upto 7

---
+++ while_exercise.py
@@ -0,0 +1,32 @@
+# 3. Write a while loop that prints the square of each number from 1 to 10.
+
+# num = 1
+
+# while num<=10:
+#     print(num*num)
+#     num = num + 1
+
+
+# 4. Write a while loop to find the sum of the first 10 natural numbers.
+
+# sum = 0
+# num= 0
+
+# while num<=10:
+#     sum = sum + num
+#     num = num + 1
+# print(sum)
+
+# 5. Use a while loop to print "Hello" 5 times.
+# count = 0
+
+# while count < 5:
+#     print("Hello")
+#     count = count + 1
+
+# 6. Write a while loop to print the multiplication table of 7.
+
+# num = 1
+# while num<=10:
+#     print(7*num)
+#     num = num + 1
